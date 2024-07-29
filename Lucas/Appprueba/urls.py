7f1368d1aca3ed597e94e02ba4a44de9b77b6b84
Lucas Carré <lukasfaridcarre@gmail.com>
2024-07-29 11:15:17 -0300

    Proyecto finalizado para la entrega

--- Lucas/Appprueba/urls.py
+++ Lucas/Appprueba/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from Appprueba import views
 
 urlpatterns = [
-    path('/', views.inicio, name='Inicio'),
+    path('', views.inicio, name='Inicio'),
     path('cursos/', views.cursos, name='Cursos'),
     path('profesores/', views.profesores, name= 'Profesores'),
     path('estudiantes/', views.estudiantes, name='Estudiantes'),
-    path('practicas/', views.practicas, name= 'Practicas')
-]
+    path('practicas/', views.practicas, name= 'Practicas'),
+    path('formulario_curso/', views.formulario_curso, name='Formulario Cursos'),
+    path('form-con-api/', views.agregar_curso_api, name="Agregar Cursos"),
+    path('buscar-cursos/', views.buscar_cursos, name="Buscar Cursos"),
+    ]
--- Lucas/Lucas/urls.py
+++ Lucas/Lucas/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Appprueba/', include('Appprueba.urls'))
+    path('', include('Appprueba.urls'))
 ]
